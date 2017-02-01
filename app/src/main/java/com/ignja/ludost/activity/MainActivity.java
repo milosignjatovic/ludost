4d6817ac05aca3a0a34e0d9f0989efb1c8c74a0d
Milos Ignjatovic <milos.ignjatovic@gmail.com>
2017-02-02 00:45:48 +0100

    Test3

---
+++ app/src/main/java/com/ignja/ludost/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ignja.ludost.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ignja.ludost.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
