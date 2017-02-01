2ee3149a2f7b12af75398603de495afa92369a01
Milos Ignjatovic <milos.ignjatovic@gmail.com>
2017-02-01 23:08:11 +0100

    Local test

--- app/build/generated/source/r/debug/com/ignja/ludost/R.java
+++ app/build/generated/source/r/debug/com/ignja/ludost/R.java
@@ -2010,7 +2010,9 @@ containing a value of this type.
     }
     public static final class raw {
         public static final int fragment_shader=0x7f060000;
-        public static final int vertex_shader=0x7f060001;
+        public static final int light_fragment_shader=0x7f060001;
+        public static final int light_vertex_shader=0x7f060002;
+        public static final int vertex_shader=0x7f060003;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f070000;
