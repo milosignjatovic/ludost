a909df4476e7043567c9f5c1f593c678b59cf8c8
Milos Ignjatovic <milos.ignjatovic@gmail.com>
2017-02-02 00:27:41 +0100

    Test

--- app/src/main/java/com/ignja/ludost/activity/OpenGLES20Activity.java
+++ app/src/main/java/com/ignja/ludost/activity/OpenGLES20Activity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ignja.ludost;
+package com.ignja.ludost.activity;
 
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
