93f36d193ee32265266e9450fcd8cb3e2b3c7074
Milos Ignjatovic <milos.ignjatovic@gmail.com>
2020-05-26 17:47:51 +0200

    Logger config uncommented

--- app/src/main/java/com/ignja/gl/util/LoggerConfig.java
+++ app/src/main/java/com/ignja/gl/util/LoggerConfig.java
@@ -9,13 +9,6 @@ public class LoggerConfig {
     /**
      * Enable application log
      *
-     * Set to FALSE because of
-     * JNI DETECTED ERROR IN APPLICATION: input is
-     * not valid Modified UTF-8: illegal start byte 0xfc...
-     * in call to NewStringUTF
-     *
-     * error with gl driver (in emulator)
-     *
      */
     public static final boolean ON = false;
 
